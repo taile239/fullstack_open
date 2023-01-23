af70824bf1386f581dbcea320ad83cd893f6ea50
taile239 <85147576+taile239@users.noreply.github.com>
2023-01-24 06:00:46 +0900

    Create Course.js

---
+++ part2/courseinfo/src/components/Course.js
@@ -0,0 +1,36 @@
+const Header = ({ course }) => <h1>{course}</h1>;
+
+const Total = ({ sum }) => (
+  <p>
+    <strong>total of {sum} exercises</strong>
+  </p>
+);
+
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+);
+
+const Content = ({ parts }) => (
+  <>
+    {parts.map((part) => (
+      <Part key={part.id} part={part} />
+    ))}
+  </>
+);
+
+const Course = ({ course }) => {
+  const sum = course.parts.reduce((s, p) => {
+    return s + p.exercises;
+  }, 0);
+  return (
+    <>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total sum={sum} />
+    </>
+  );
+};
+
+export default Course;
