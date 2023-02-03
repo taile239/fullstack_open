34a00288e439099cfc31da3d825f66f9d6b556be
taile239 <85147576+taile239@users.noreply.github.com>
2023-02-03 09:34:26 +0700

    Create Country.js

---
+++ part2/countries/src/components/Country.js
@@ -0,0 +1,47 @@
+import countriesServices from "./../services/countries";
+import { useState, useEffect } from "react";
+
+const Country = ({ country, isShow }) => {
+  const [weather, setWeather] = useState(null);
+
+  useEffect(() => {
+    countriesServices.getWeatherForecast(country).then((returnedWeather) => {
+      console.log(returnedWeather);
+      setWeather(returnedWeather);
+    });
+  }, [country]);
+
+  if (!isShow) {
+    return;
+  }
+
+  let celcius, windSpeed;
+
+  if (weather) {
+    celcius = (5 / 9) * (weather.current.temp - 32);
+    windSpeed = weather.current.wind_speed;
+  }
+
+  return (
+    <>
+      <h1>{country.name["common"]}</h1>
+      <div>capital {country.capital}</div>
+      <div>area {country.area}</div>
+      <br />
+      <div>
+        <strong>languages:</strong>
+      </div>
+      <ul>
+        {Object.values(country.languages).map((value, index) => {
+          return <li key={index}>{value}</li>;
+        })}
+      </ul>
+      <img src={country.flags.svg} height="100" alt="country" />
+      <h1>Weather in {country.capital}</h1>
+      <div>temperature {celcius} Celcius</div>
+      <div>wind {windSpeed} m/s</div>
+    </>
+  );
+};
+
+export default Country;
